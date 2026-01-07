53fa93d68c6e1a0d30c4acf5ab553fc37869d20a
Mohi Redoy <redoyahmed35@gmail.com>
2026-01-07 22:27:43 +0600

    7.1.26

---
+++ A_Twins.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define nl "\n"
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define rrep(i, a, b) for(int i = (a); i >= (b); i--)
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef vector <double> vd;
+typedef vector <ll> vll;
+
+void solve()
+{
+    int n, ts = 0, s = 0;
+    cin >> n;
+
+    int a[n];
+
+    rep(i,0,n)
+    {
+        cin >> a[i];
+        ts += a[i];
+    }
+    sort(a,a+n);
+
+    int cnt = 0;
+    rep(i,0,n)
+    {
+        cnt++;
+        ts -= a[i];
+        s += a[i];
+        if(s > ts)
+            break;
+    }
+
+    cout << cnt << endl;
+}
+
+int main ()
+{
+    optimize();
+              
+    int t = 1;
+    //cin >> t;
+
+    while(t--)
+        solve();
+        
+    return 0;
+}
