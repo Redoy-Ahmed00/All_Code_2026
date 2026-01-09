65728f0b663aedc65a3b5c26d0bd4f1bae49a3db
Mohi Redoy <redoyahmed35@gmail.com>
2026-01-09 21:16:09 +0600

    1.9.26

---
+++ A_Yes_or_Yes.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define nl "\n"
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define rrep(i, a, b) for(int i = (a); i >= (b); i--)
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef vector <double> vd;
+typedef vector <ll> vll;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    int n = s.size();
+    int y = count(s.begin(), s.end(), 'Y');
+
+    if (y > 1) 
+        cout << "NO" << endl;
+    else 
+        cout << "YES" << endl;
+
+}
+
+int main ()
+{
+    optimize();
+              
+    int t = 1;
+    cin >> t;
+
+    while(t--)
+        solve();
+        
+    return 0;
+}
