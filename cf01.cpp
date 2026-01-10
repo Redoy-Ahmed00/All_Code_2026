a917b1c25c2946cb6f9a4aac33ca4d159ec72dfc
M. K. A MOHI REDOY <redoyahmed35@gmail.com>
2026-01-10 17:20:45 +0600

    Update cf01.cpp

--- cf01.cpp
+++ cf01.cpp
@@ -1,38 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define nl "\n"
-#define rep(i, a, b) for(int i = (a); i < (b); i++)
-#define rrep(i, a, b) for(int i = (a); i >= (b); i--)
-
-typedef long long ll;
-typedef vector <int> vi;
-typedef vector <double> vd;
-typedef vector <ll> vll;
-
-void solve()
+int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int a, b, c;
-    
     cin >> a >> b >> c;
-    
-    int ans = (a+b)*c;
-    ans = max(ans, a*b*c);
-    ans = max(ans, a*(b+c));
-    
-    cout << ans << endl;
-}
 
-int main ()
-{
-    optimize();
-              
-    int t = 1;
-    //cin >> t;
+    int ans = max({
+        a + b + c,
+        a + b * c,
+        a * b + c,
+        (a + b) * c,
+        a * (b + c),
+        a * b * c
+    });
 
-    while(t--)
-        solve();
-        
+    cout << ans << "\n";
     return 0;
 }
