f70d1c2945eaf4025101f8e747c150f906a76ced
Mohi Redoy <redoyahmed35@gmail.com>
2026-01-15 20:31:36 +0600

    15.1.26

---
+++ A_Kefa_and_First_Steps.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define nl "\n"
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define rrep(i, a, b) for(int i = (a); i >= (b); i--)
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef vector <double> vd;
+typedef vector <ll> vll;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    rep(i,0,n)
+        cin >> arr[i];
+    int ops = 1, mx = 1;
+    rep(i,1,n)
+    {
+        if(arr[i] >= arr[i-1])
+            ops++;
+        else
+            ops = 1;
+        mx = max(ops, mx);
+    }
+    cout << mx << endl;
+
+}
+
+int main ()
+{
+    optimize();
+              
+    int t = 1;
+    //cin >> t;
+
+    while(t--)
+        solve();
+        
+    return 0;
+}
