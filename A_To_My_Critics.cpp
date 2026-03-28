e869cae4ce6600127084fd1f07fba7060b90c425
Mohi Redoy <redoyahmed35@gmail.com>
2026-03-28 22:31:17 +0600

    28.3.26

---
+++ A_To_My_Critics.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define nl "\n"
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define rrep(i, a, b) for(int i = (a); i >= (b); i--)
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef vector <double> vd;
+typedef vector <ll> vll;
+
+void solve()
+{
+    int a, b, c;
+    cin >> a>> b >> c;
+
+    if(a+b>=10 || b+c >= 10 || c+a >= 10)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+
+int main ()
+{
+    optimize();
+              
+    int t = 1;
+    cin >> t;
+
+    while(t--)
+        solve();
+        
+    return 0;
+}
