061917ab53c6fecfffb43db043ce34bdadaca5a0
Mohi Redoy <redoyahmed35@gmail.com>
2026-03-29 21:40:17 +0600

    29.3.26

---
+++ B_Simply_Sitting_on_Chairs.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define nl "\n"
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define rrep(i, a, b) for(int i = (a); i >= (b); i--)
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef vector <double> vd;
+typedef vector <bool> vb;
+typedef vector <ll> vll;
+
+void solve()
+{
+    int n, cnt = 0;
+    cin >> n;
+
+    vi v(n+1);
+    vb marked(n+1, false);
+
+    rep(i,1,n+1)
+        cin >> v[i];
+        
+    rep(i,1,n+1)
+    {
+        if(marked[i] == true)
+            break;
+        cnt++;
+        marked[v[i]] = true;
+        
+    }
+
+    cout << cnt << endl;
+        
+}
+
+int main ()
+{
+    optimize();
+              
+    int t = 1;
+    cin >> t;
+
+    while(t--)
+        solve();
+        
+    return 0;
+}
