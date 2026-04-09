1169eb5f8f3506a390f278329720947c9d9accbe
Mohi Redoy <redoyahmed35@gmail.com>
2026-04-09 23:01:41 +0600

    9.4.26

--- B_Collecting_Game.cpp
+++ B_Collecting_Game.cpp
@@ -13,7 +13,33 @@ typedef vector <ll> vll;
 
 void solve()
 {
+    int n;
+    cin >> n;
+    vll v(n), tmp(n) ,ps(n), ans(n), ahead(n);
+    rep(i,0,n)
+    {
+        cin >> v[i];
+        tmp[i] = v[i];
+    }
+    sort(tmp.begin(),tmp.end());
+    ps[0] = tmp[0];
+    rep(i,1,n)
+        ps[i] = ps[i-1]+tmp[i];
+
     
+    rep(i,0,n)
+    {
+        int idx = (lower_bound(tmp.begin(), tmp.end(), v[i])) - tmp.begin();
+
+        while(idx + 1 < n && ps[idx] >= tmp[idx+1])
+            idx++;
+
+        ans[i] = idx;
+    }
+
+    for(auto x: ans)
+        cout << x << " ";
+    cout << endl;
 }
 
 int main ()
